b74112a7f1b7d2b25df8c1c9c82c82919d47485c
dkibrahi <dkibrahi@umich.edu>
2023-02-24 17:04:36 -0500

    created script for input box

---
+++ Assets/Scripts/Dialogue_Scripts/Input_Box.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+
+public class Input_Box : MonoBehaviour
+{
+
+    public GameObject inputField;
+
+    public virtual void Start() {
+        inputField.gameObject.SetActive(false);
+    }
+
+    public void handleDisplay() {
+        bool isDisplayed = inputField.gameObject.activeSelf;
+
+        inputField.gameObject.SetActive(!isDisplayed);
+    }
+
+}
