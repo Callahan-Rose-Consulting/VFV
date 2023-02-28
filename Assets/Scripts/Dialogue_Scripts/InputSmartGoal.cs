795b5e54e2a3503a4112941b71a3ff671c675799
unknown <mohsenc@umich.edu>
2023-02-28 18:10:42 -0500

    stopping player movement

---
+++ Assets/Scripts/Dialogue_Scripts/InputSmartGoal.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputSmartGoal : MonoBehaviour
+{
+    public string smartGoal;
+    public GameObject inputField;
+    public GameObject textDisplay;
+    public PlayerMovement playerMovement;
+
+    private void Start()
+    {
+        playerMovement = FindObjectOfType<PlayerMovement>();
+    }
+
+    public void GetSmartGoal()
+    {
+        // Disable player movement
+        playerMovement.enabled = false;
+
+        smartGoal = inputField.GetComponent<Text>().text;
+        textDisplay.GetComponent<Text>().text = "You entered: " + smartGoal;
+    }
+
+    public void OnInputFieldEndEdit()
+    {
+        // Enable player movement
+        playerMovement.enabled = true;
+    }
+}
