fb7adf8b1b4b2af126eb8d36ef633d8409412109
unknown <mohsenc@umich.edu>
2023-03-07 12:04:00 -0500

    trying to add the video to the begining of the game

---
+++ Assets/Scripts/VideoPlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+
+public class VideoPlayerController : MonoBehaviour
+{
+    private VideoPlayer videoPlayer;
+
+    private void Awake()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+        videoPlayer.loopPointReached += EndReached;
+    }
+
+    void Start()
+    {
+        videoPlayer.Play();
+    }
+
+    void EndReached(UnityEngine.Video.VideoPlayer vp)
+    {
+        SceneManager.LoadScene("NewMainMenu");
+    }
+}
+
+
